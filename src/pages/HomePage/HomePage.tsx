ac359b785adf5f4cd69b42cfbbb55502818a636e
Naresh Bhatia <me@nareshbhatia.dev>
2022-10-05 18:29:32 -0400

    Changed example to financial domain to show more use cases

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,72 +1,151 @@
 import * as React from 'react';
 import {
-  ColDef,
+  GetGroupRowAggParams,
+  GridOptions,
+  GridReadyEvent,
   ValueFormatterParams,
-  ValueGetterParams,
 } from 'ag-grid-community';
+import 'ag-grid-enterprise';
 import { AgGridReact } from 'ag-grid-react';
-import { evaluate } from 'mathjs';
-
+// import { evaluate } from 'mathjs';
 import numeral from 'numeral';
 import { Header } from '../../components';
+import { lots } from './data';
+
+const PriorityEnum: { [key: string]: number } = {
+  L: 0,
+  M: 1,
+  H: 2,
+};
 
 const formatNumber = (params: ValueFormatterParams) => {
   return numeral(params.value).format('0,0.00');
 };
 
-const calcCircumference = (params: ValueGetterParams) => {
-  return 2 * Math.PI * params.data.r;
-};
-
-const evaluateExpression = (params: ValueGetterParams) => {
-  return evaluate('pi r ^ 2', params.data);
-};
-
-const columnDefs: Array<ColDef> = [
-  { field: 'r', headerName: 'Radius', type: 'rightAligned' },
+const columnDefs = [
+  { field: 'symbol', headerName: 'Symbol', rowGroup: true, hide: true },
+  { field: 'id', headerName: 'Lot #', type: 'rightAligned' },
   {
-    field: 'circumference',
-    headerName: 'Circumference',
-    valueGetter: calcCircumference,
-    valueFormatter: formatNumber,
-    type: 'rightAligned',
+    field: 'priority',
+    headerName: 'Priority',
+    comparator: (valueA: string, valueB: string) =>
+      PriorityEnum[valueA] - PriorityEnum[valueB],
   },
   {
-    field: 'area-aggrid',
-    headerName: 'Area (ag-grid)',
-    valueGetter: 'Math.PI * data.r ** 2',
-    valueFormatter: formatNumber,
+    field: 'quantity',
+    colId: 'quantity',
+    headerName: 'Quantity',
     type: 'rightAligned',
+    editable: true,
   },
   {
-    field: 'area-mathjs',
-    headerName: 'Area (mathjs)',
-    valueGetter: evaluateExpression,
+    headerName: 'Buy',
+    children: [
+      { field: 'buy.date', headerName: 'Date' },
+      {
+        field: 'buy.price',
+        colId: 'buyPrice',
+        headerName: 'Price',
+        valueFormatter: formatNumber,
+        type: 'rightAligned',
+        editable: true,
+      },
+      {
+        colId: 'buyAmount',
+        headerName: 'Amount',
+        valueGetter: 'getValue("quantity") * getValue("buyPrice")',
+        valueFormatter: formatNumber,
+        type: 'rightAligned',
+      },
+    ],
+  },
+  {
+    headerName: 'Sell',
+    children: [
+      { field: 'sell.date', headerName: 'Date' },
+      {
+        field: 'sell.price',
+        colId: 'sellPrice',
+        headerName: 'Price',
+        valueFormatter: formatNumber,
+        type: 'rightAligned',
+        editable: true,
+      },
+      {
+        colId: 'sellAmount',
+        headerName: 'Amount',
+        valueGetter: 'getValue("quantity") * getValue("sellPrice")',
+        valueFormatter: formatNumber,
+        type: 'rightAligned',
+      },
+    ],
+  },
+  {
+    field: 'gain',
+    headerName: 'Gain',
+    valueGetter: 'getValue("sellAmount") - getValue("buyAmount")',
     valueFormatter: formatNumber,
     type: 'rightAligned',
+    cellClassRules: {
+      loss: 'x < 0',
+      profit: 'x > 0',
+    },
   },
 ];
 
 export function HomePage() {
-  const [rowData] = React.useState([
-    { r: 1 },
-    { r: 2 },
-    { r: 3 },
-    { r: 4 },
-    { r: 5 },
-    { r: 6 },
-    { r: 7 },
-    { r: 8 },
-    { r: 9 },
-    { r: 10 },
-  ]);
+  const [rowData] = React.useState(lots);
+
+  const handleGridReady = (event: GridReadyEvent) => {
+    // save gridApi and columnApi in context so that other components can use it
+    const { api: gridApi } = event;
+    gridApi.sizeColumnsToFit();
+  };
+
+  const defaultColDef = {
+    resizable: true,
+    sortable: true,
+  };
+
+  const gridOptions: GridOptions = {
+    suppressCellSelection: true,
+    defaultColDef,
+    columnDefs,
+    rowData,
+    onGridReady: handleGridReady,
+  };
+
+  const getGroupRowAgg = React.useCallback((params: GetGroupRowAggParams) => {
+    const result = {
+      quantity: 0,
+      buyAmount: 0,
+      sellAmount: 0,
+      buyPrice: 0,
+      sellPrice: 0,
+    };
+    params.nodes.forEach((node) => {
+      if (node.group === false) {
+        const data = node.data;
+        result.quantity += data.quantity;
+        // TODO: how to get buy amount from calculated field instead of calculating again?
+        result.buyAmount += data.quantity * data.buy.price;
+        result.sellAmount += data.quantity * data.sell.price;
+      }
+    });
+    result.buyPrice = result.buyAmount / result.quantity;
+    result.sellPrice = result.sellAmount / result.quantity;
+    return result;
+  }, []);
 
   return (
     <div className="h-screen flex flex-col">
       <Header />
       <div className="p-3 flex-1">
         <div className="ag-theme-alpine h-full w-full">
-          <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
+          <AgGridReact
+            gridOptions={gridOptions}
+            getGroupRowAgg={getGroupRowAgg}
+          />
         </div>
       </div>
     </div>
---
+++ src/pages/HomePage/data.ts
@@ -0,0 +1,72 @@
+export const lots = [
+  {
+    id: 1265,
+    symbol: 'TSLA',
+    quantity: 100,
+    priority: 'H',
+    buy: {
+      date: '2021-07-09',
+      price: 598,
+    },
+    sell: {
+      date: '2021-07-12',
+      price: 599,
+    },
+  },
+  {
+    id: 2394,
+    symbol: 'AAPL',
+    quantity: 200,
+    priority: 'L',
+    buy: {
+      date: '2021-08-10',
+      price: 127,
+    },
+    sell: {
+      date: '2021-08-17',
+      price: 126,
+    },
+  },
+  {
+    id: 6879,
+    symbol: 'TSLA',
+    quantity: 100,
+    priority: 'M',
+    buy: {
+      date: '2021-08-21',
+      price: 600,
+    },
+    sell: {
+      date: '2021-08-25',
+      price: 600,
+    },
+  },
+  {
+    id: 5397,
+    symbol: 'TSLA',
+    quantity: 100,
+    priority: 'H',
+    buy: {
+      date: '2021-08-22',
+      price: 599,
+    },
+    sell: {
+      date: '2021-08-25',
+      price: 600,
+    },
+  },
+  {
+    id: 6971,
+    symbol: 'AAPL',
+    quantity: 100,
+    priority: 'H',
+    buy: {
+      date: '2021-09-01',
+      price: 126,
+    },
+    sell: {
+      date: '2021-09-12',
+      price: 130,
+    },
+  },
+];
